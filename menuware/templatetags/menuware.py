1c63827c13e25a68b9fa70bc462b8276573c7c68
Diana GarCo <dianagarco@gmail.com>
2021-02-01 19:57:10 -0500

    Update menuware.py

--- menuware/templatetags/menuware.py
+++ menuware/templatetags/menuware.py
@@ -7,7 +7,7 @@ from .. import defaults as defs
 register = template.Library()
 
 
-@register.assignment_tag(takes_context=True)
+@register.simple_tag(takes_context=True)
 def get_menu(context, menu_name):
     """
     Returns a consumable menu list for a given menu_name found in settings.py.
